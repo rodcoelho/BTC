ed4408f15c64c35f36bdfe1dfb04d7cdb0f24d06
rodrigo coelho <rodrigocoelho@rodrigos-MacBook-Pro.local>
2017-11-08 18:01:39 -0500

    updated delta function, improved variable names. Next expected update: 1) store data points outside .py file and 2) cron tabs prep.

--- btc_api.py
+++ btc_api.py
@@ -1,4 +1,4 @@
-import sys
+import sys, time
 import requests
 #(final step) from sendmessages import sendtext
 
@@ -6,35 +6,29 @@ url = "https://api.coindesk.com/v1/bpi/currentprice.json"
 #The relevant data is scraped from CoinDesk API - 'Powered by CoinDesk'
 #CoinDesk website: https://www.coindesk.com/price/
 
-BTC_price_list = []
-#list used to store recent price points
+# temporary solution: sys.argv[1] will be the time interval - or how often - the user will want to check
+time_interval = (float(sys.argv[1]) * 60 *60)
 
-def check_list_len():
-    #checks length of list, if there are more than 12 data points, it will throw out old data
-    if len(BTC_price_list) > 12:
-        BTC_price_list[:] = BTC_price_list[:2]
-
-def extractAndSaveBTC():
+def get_latest_BTC():
     #uses requests to pull data from CoinDesk, returns Bitcoin price (USD)
     BTCinfo = requests.get(url).json()
     return BTCinfo['bpi']['USD']['rate_float']
 
-def add_new_price_to_list():
-    #adds new data point to front of list
-    x = extractAndSaveBTC()
-    BTC_price_list.insert(0,x)
-
-def check_price_delta():
-    #simple price change formula
-    delta = (BTC_price_list[0] - BTC_price_list[1]) / (BTC_price_list[1])
-    return delta
+def check_price_delta(latestBTC, oldBTC):
+    x = (latestBTC - oldBTC) / (oldBTC)
+    if x != 0 or x != None:
+        return x*100
+    else:
+        return 'zero change - or more likely, there is an error'
 
 def main():
-    #main function, not yet finished
-    check_list_len()
-    add_new_price_to_list()
-    if len(BTC_price_list) > 1:
-        recent_delta = check_price_delta()
+    latestBTC = get_latest_BTC()
+    time.sleep(time_interval) #need to take this out and use cron tabs
+                                #first, need to find way to store data points outside of .py file
+    oldBTC = latestBTC
+    latestBTC = get_latest_BTC()
+    delta = check_price_delta(latestBTC,oldBTC)
+    print('{}'.format(delta))
 
 if __name__ == "__main__":
     main()
